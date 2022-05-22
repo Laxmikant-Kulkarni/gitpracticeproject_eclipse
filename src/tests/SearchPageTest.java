5b34ec5ca6a02f96fd86e0b7dc4ee954f506f703
Laxmikant-Kulkarni <lvkulkarni2000@gmail.com>
2022-05-22 21:06:28 +0200

    commiting search page test

---
+++ src/tests/SearchPageTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class SearchPageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Tittle - Search Page - Test");
+		System.out.println("Search Page Test Updated");
+
+	}
+
+}
