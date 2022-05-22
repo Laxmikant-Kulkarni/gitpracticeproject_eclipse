bc78f9c9b95dce69031f9294255bf412724c23f4
Laxmikant Kulkarni <96416529+Laxmikant-Kulkarni@users.noreply.github.com>
2022-05-23 00:27:24 +0200

    Create RegistrationPageTest.java

---
+++ src/tests/RegistrationPageTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class RegistrationPageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int r2 = 10;
+		System.out.println(r2);
+
+	}
+
+}
