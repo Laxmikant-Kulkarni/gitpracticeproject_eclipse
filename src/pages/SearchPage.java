ad47a94fde56f60e20da300ea46b9772d80f2b93
Laxmikant-Kulkarni <lvkulkarni2000@gmail.com>
2022-05-22 19:12:32 +0200

    Adding Search Page and Updating Login Page test

---
+++ src/pages/SearchPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Tittle - SearchPage");
+		
+		
+	}
+
+}
--- src/tests/LoginPageTest.java
+++ src/tests/LoginPageTest.java
@@ -6,6 +6,7 @@ public class LoginPageTest {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Tittle - LoginPage - Test");
+		System.out.println("Login Page Test Updated");
 		
 	}
 
