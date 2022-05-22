04bccb5876373314933a607d4171d63723f6c947
Laxmikant-Kulkarni <lvkulkarni2000@gmail.com>
2022-05-23 00:04:36 +0200

    Committting classic page file

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+public class ClassicPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Title - Classic Page Updated");
+	    int x =10;
+	    int y =20;
+	    int sum2 = x + y ;
+	    
+	    System.out.println("Sum"+sum2);
+
+	}
+
+}
