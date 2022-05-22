50e4b2540f8ecb97e1dd962cace9669696fec522
Laxmikant-Kulkarni <lvkulkarni2000@gmail.com>
2022-05-22 21:24:23 +0200

    Commiting registration page

---
+++ src/pages/RegistrationPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class RegistrationPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Registration Page");
+		String name = "Laxmikant";
+		String company = "epam";
+		int id = 10;
+
+	}
+
+}
