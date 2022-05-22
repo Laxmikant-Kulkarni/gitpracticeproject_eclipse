e3d4e728d7713525d5324b9104674c682efd0c78
Laxmikant Kulkarni <96416529+Laxmikant-Kulkarni@users.noreply.github.com>
2022-05-23 00:01:57 +0200

    Create ClassicPage.java

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+public class ClassicPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Title - Classic Page");
+    int a =10;
+    int b =20;
+    int sum = a + b ;
+    
+    System.out.println("Sum"+sum);
+
+	}
+
+}
