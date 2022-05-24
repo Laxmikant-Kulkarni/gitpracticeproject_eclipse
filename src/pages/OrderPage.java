ca7d3c0f858b26dba4a3aee6df9830a8de56b6c1
Shraddha-M-Joshi <shraddhajoshi541@gmail.com>
2022-05-24 22:29:32 +0200

    SK -   Commiting order page and test

---
+++ src/pages/OrderPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class OrderPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Title -  ORDER PAGE");
+		
+		System.out.println("SK -  Working on the order page");
+
+	}
+
+}
